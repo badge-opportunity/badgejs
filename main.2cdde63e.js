bd324509df6161179318a7a796f6837cab236b1c
badge-opportunity <minfringementcopyrights@gmail.com>
2025-01-19 21:47:06 +0500

    Update main.2cdde63e.js

--- main.2cdde63e.js
+++ main.2cdde63e.js
@@ -11608,16 +11608,6 @@
                         className: "d-flex flex-column",
                         children: [(0,
                         Le.jsx)("input", {
-                            className: "p-1 border rounded",
-                            type: "text",
-                            name: "PAge Url",
-                            placeholder: "Enter page url",
-                            required: !0,
-                            style: {
-                                boxShadow: "0 0.5rem 1rem rgba(0, 0, 0, 0.15)"
-                            }
-                        }), (0,
-                        Le.jsx)("input", {
                             className: "mt-4 p-1 border rounded",
                             name: "password",
                             type: "password",
